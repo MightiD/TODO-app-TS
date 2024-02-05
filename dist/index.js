a8b58207884d0d4a7edad016c219111742ac1a17
MightiD <128378210+MightiD@users.noreply.github.com>
2024-02-05 20:57:30 +0000

    Handles Input Better & forces user to input in both fields

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,21 @@
 "use strict";
 const form = document.getElementById("form");
+let todoList = [];
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const titleInput = document.getElementById("title");
-    const title = titleInput.value;
-    console.log(title);
     const contentsInput = document.getElementById("contents");
-    const contents = contentsInput.value;
-    console.log(contents);
+    const correctInput = document.getElementById("correctInput");
+    if (!titleInput.value || !contentsInput.value) {
+        correctInput.textContent = "You Must Enter an Input in Both Fields";
+    }
+    else {
+        correctInput.textContent = "";
+        const title = titleInput.value;
+        const contents = contentsInput.value;
+        let item = { title, contents };
+        todoList.push(item);
+    }
+    ;
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,45 @@
+//creates type for items
+type Item = {
+    title: string,
+    contents: string
+};
+
+//creates the full ToDo list
+let todoList: Item[] = [];
+
+//gets the html form
 const form = document.getElementById("form") as HTMLFormElement;
 
+//listens for the submit button being pressed
 form.addEventListener("submit", (event) => {
+
+    //prevents default input being registered
     event.preventDefault();
 
+    //gets all the necessary html elements
     const titleInput = document.getElementById("title") as HTMLInputElement;
-    const title = titleInput.value;
-
-    console.log(title);
-
     const contentsInput = document.getElementById("contents") as HTMLInputElement;
-    const contents = contentsInput.value;
+    const correctInput = document.getElementById("correctInput") as HTMLDivElement;
 
-    console.log(contents);
+    //checks if both inputs have a value
+    if (!titleInput.value || !contentsInput.value){
+        //sets div to this
+        correctInput.textContent = "You Must Enter an Input in Both Fields";
+    }
+    else{
+        //sets div to this
+        correctInput.textContent = "";
+    
+        //gets input values
+        let title = titleInput.value;
+        let contents = contentsInput.value;
 
+        //created var with those items
+        let item: Item = {title, contents};
+
+        //adds it to a list
+        todoList.push(item);
+
+        todoList.forEach(i => console.log(`Title: ${i.title} \nContents: ${i.contents}`))
+    };
 });
